68cdc53c595d609acecbeebc8dec09c56b06f965
Zain Ul Hassan <zainprog56@gmail.com>
2026-06-20 13:30:00 +0800

    update the config file for live host page

--- conference-website/vite.config.js
+++ conference-website/vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/conference_website_asia/",
   plugins: [react(), tailwindcss()],
 });
